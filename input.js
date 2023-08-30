f161a77bb4aa9bfa2f44ea5b0defe517e57e8da0
Justin Hao <justinhao@live.com>
2023-08-30 16:55:17 -0400

    Add directional input

--- input.js
+++ input.js
@@ -1,5 +1,8 @@
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -11,6 +14,21 @@ const setupInput = function () {
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit()
+  }
+  if (key === 'w') {
+    connection.write("Move: up")
+  }
+  if (key === 's') {
+    connection.write("Move: down")
+
+  }
+  if (key === 'a') {
+    connection.write("Move: left")
+
+  }
+  if (key === 'd') {
+    connection.write("Move: right")
+
   }
 }
 

29b0ea3b02940d66c279e62fe871447e8de84a6c
Justin Hao <justinhao@live.com>
2023-08-30 16:13:24 -0400

    Create and export from input.js

---
+++ input.js
@@ -0,0 +1,19 @@
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit()
+  }
+}
+
+module.exports = {
+  setupInput,
+}

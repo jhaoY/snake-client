e3b736e3fbf9140166575540ada06f0a74f0f12e
Justin Hao <justinhao@live.com>
2023-08-30 16:09:17 -0400

    Add input functionality and ctrl+c to exit

--- play.js
+++ play.js
@@ -1,5 +1,22 @@
 const net = require("net");
 const {connect} = require("./client")
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on("data", handleUserInput);
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit()
+  }
+}
+
 console.log("Connecting ...");
 connect();
+setupInput();
